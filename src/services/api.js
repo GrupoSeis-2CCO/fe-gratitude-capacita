31b4f49cfbada6ef2093a262c07b4d1fb0b8dd8c
Nícolas Reis <160797124+nicolas-rlopes@users.noreply.github.com>
2025-10-26 14:50:24 -0300

    Refactor api.js by removing commented code
    Removed unused request interceptor code and cleaned up comments.

--- src/services/api.js
+++ src/services/api.js
@@ -9,29 +9,6 @@ export const api = axios.create({
   baseURL: BASE_URL,
 });
 
-// Normalize relative URLs so baseURL is applied even if callers pass a leading '/'
-api.interceptors.request.use((config) => {
-  const url = config.url || '';
-  const isAbsolute = /^https?:\/\//i.test(url);
-  if (!isAbsolute && url.startsWith('/')) {
-    config.url = url.replace(/^\/+/, '');
-  }
-  return config;
-});
-
-// Config Local
-/*
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-*/
-
-// Config AWS
-
 api.interceptors.request.use((config) => {
   // Não adicionar token APENAS para o endpoint de login
   const isLoginEndpoint = (config.url || '').includes('usuarios/login');
@@ -45,6 +22,5 @@ api.interceptors.request.use((config) => {
 
   return config;
 });
-
 
 export default api;
