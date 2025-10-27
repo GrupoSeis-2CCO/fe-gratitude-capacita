3b27dbf6830ac9fe3e0228d1f7f137acba8f82db
MatheusRequena <matheus.requena@sptech.school>
2025-10-27 18:52:14 -0300

    feat: using cookies to save previous page and only save on stack when the page change

--- src/components/NavigationTracker.jsx
+++ src/components/NavigationTracker.jsx
@@ -1,25 +1,38 @@
-import { useEffect, useRef } from "react";
+import { useLayoutEffect } from "react";
 import { useLocation } from "react-router-dom";
 import useHistory from "../hooks/useHistory.js";
+
+const COOKIE_NAME = "last_page";
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+    return parts.pop().split(";").shift();
+  }
+  return null;
+}
+
+function setCookie(name, value) {
+  document.cookie = `${name}=${value}; path=/; max-age=86400; SameSite=Strict`;
+}
 
 export default function NavigationTracker({ children }) {
   const location = useLocation();
   const { push, peek } = useHistory();
-  const lastProcessedPath = useRef(null);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const currentPath = `${location.pathname}${location.search}${location.hash}`;
-    const lastInStack = peek();
+    const cookieValue = getCookie(COOKIE_NAME);
 
-    if (
-      lastProcessedPath.current === currentPath ||
-      lastInStack === currentPath
-    ) {
-      return;
+    if (cookieValue && cookieValue !== currentPath) {
+      const lastInStack = peek();
+      if (lastInStack !== cookieValue) {
+        push(cookieValue);
+      }
     }
 
-    push(currentPath);
-    lastProcessedPath.current = currentPath;
+    setCookie(COOKIE_NAME, currentPath);
   }, [location.pathname, location.search, location.hash, push, peek]);
 
   return children;
--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,10 +1,7 @@
-import ReturnArrow from "../components/ReturnArrow";
-
 function TestPage() {
   return (
-    <div className="relative min-h-screen flex flex-col px-8 pt-24 pb-20">
-      <ReturnArrow />
-    </div>
+    <>
+    </>
   );
 }
 
