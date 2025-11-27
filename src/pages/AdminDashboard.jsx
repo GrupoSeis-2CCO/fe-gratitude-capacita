fd7aa55d38a2565010830c688b345124888e37f3
Alexandre Vasconcelos Nichio Junior <160503435+AlexandreVNJR@users.noreply.github.com>
2025-11-27 18:39:35 -0300

    Rename AdminDashboard component to AdminDashboardPage

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,8 +1,7 @@
 
-
 import React from 'react';
 
-export default function AdminDashboard() {
+export default function AdminDashboardPage() {
 	return (
 		<div className="flex items-center justify-center h-full p-12">
 			<div className="bg-white rounded-lg shadow-md px-6 py-8 text-center">
